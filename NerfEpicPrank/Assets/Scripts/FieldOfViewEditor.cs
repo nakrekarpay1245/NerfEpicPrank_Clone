0dca057005dbb7444fd556ccdf73cb08aee8aaff
nakrekarpay1245 <rknyprk2001@gmail.com>
2022-02-07 15:18:10 +0300

    07.02.2022
    First Commit

---
+++ NerfEpicPrank/Assets/Scripts/FieldOfViewEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Unity;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        FieldOfView fieldOfView = (FieldOfView)target;
+
+        Handles.color = Color.white;
+
+        Handles.DrawWireArc(fieldOfView.transform.position, Vector3.up, Vector3.forward, 360, fieldOfView.viewRadius);
+        Vector3 viewAngleA = fieldOfView.DirFromAngle(-fieldOfView.viewAngle / 2, false);
+        Vector3 viewAngleB = fieldOfView.DirFromAngle(fieldOfView.viewAngle / 2, false);
+
+        Handles.DrawLine(fieldOfView.transform.position, fieldOfView.transform.position + viewAngleA * fieldOfView.viewRadius);
+        Handles.DrawLine(fieldOfView.transform.position, fieldOfView.transform.position + viewAngleB * fieldOfView.viewRadius);
+
+        Handles.color = Color.red;
+        if (fieldOfView.targetIsDetected)
+            Handles.DrawLine(fieldOfView.transform.position, fieldOfView.targetPosition);
+    }
+}
